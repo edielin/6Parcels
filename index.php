19977d5fcda9fe0a4ed1f72c4d3520530626ec33
edie <edie.lin@kaisquare.com.tw>
2017-06-12 19:30:30 +0800

    [RESTART] 6Parcels web restart

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+    include_once('config.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>6Parcels - Courier 3.0</title>
+	<meta property="og:title" content="6Parcels - Courier 3.0" />
+	<meta name="description" content="6Parcel is a cloud-service designed for courier companies to easily manage their business from day one.">
+	<meta property="og:description" content="6Parcel is a cloud-service designed for courier companies to easily manage their business from day one.">
+    <meta name="keywords" content="courier software, dispatch software, courier solution, delivery software">
+	<meta property="fb:app_id" content="" />
+	<meta property="og:url" content="http://www.6parcels.com" />
+	<meta property="og:type" content="website" />
+	<meta property="og:image" content="">
+    <meta name="robots" content="index, follow" />
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1.0, user-scalable=no">
+    <meta name="format-detection" content="telephone=no">
+    <link rel="shortcut icon" href="<?= $config['cdn_loc'] ?>images/favicon.ico?v=<?= $config['version'] ?>">
+    <link rel="stylesheet" href="<?= $config['cdn_loc'] ?>style/css/font-awesome.min.css?v=<?= $config['version'] ?>">
+    <link rel="stylesheet" href="<?= $config['cdn_loc'] ?>style/css/bootstrap.min.css?v=<?= $config['version'] ?>">
+	<link rel="stylesheet" href="<?= $config['cdn_loc'] ?>style/css/main.css?v=<?= $config['version'] ?>">
+</head>
+
+<body>
+	
+</body>
+</html>
